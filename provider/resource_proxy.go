75f455a52634e1e4d93120a09015043c7a82de4e
johnnytan2885 <johnnytan2885@gmail.com>
2025-01-08 15:30:06 +0800

    Update resource_proxy.go

--- provider/resource_proxy.go
+++ provider/resource_proxy.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
 
-	"github.com/tpretz/go-zabbix-api"
+	"github.com/johnnytan2885/go-zabbix-api"
 )
 
 // proxySchemaBase base proxy schema
